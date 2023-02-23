f17eb30f9b4f47868eab719a21a476714a93daf1
Harsha-C2508 <harshac2508@gmail.com>
2023-02-23 09:30:59 +0530

    added color picker part

--- src/component/Colorpicker.jsx
+++ src/component/Colorpicker.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
+import React from 'react'
 
-// const Colorpicker = ({setBg,bg}) => {
-//     const handleColorChange = (event) => {
-//         setBg(event.target.value);
-//       };
-//   return (
-//     <div>
-//         <label htmlFor="colorPicker">Color Picker : </label>
-//         <input
-//           id="colorPicker"
-//           type="color"
-//           value={bg}
-//           onChange={handleColorChange}
-//         />
-//       </div>
-//   )
-// }
+const Colorpicker = ({setBg,bg}) => {
+    const handleColorChange = (event) => {
+        setBg(event.target.value);
+      };
+  return (
+    <div>
+        <label htmlFor="colorPicker">Color Picker : </label>
+        <input
+          id="colorPicker"
+          type="color"
+          value={bg}
+          onChange={handleColorChange}
+        />
+      </div>
+  )
+}
 
-// export default Colorpicker
+export default Colorpicker
